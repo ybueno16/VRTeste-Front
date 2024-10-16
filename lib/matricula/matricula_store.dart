716dc2a6191fb00fa7af67fc526a74d6f1cd3a80
ybueno16 <yuribueno27@gmail.com>
2024-10-15 23:27:09 -0300

    feat: Adicionar CRUD aluno

---
+++ lib/matricula/matricula_store.dart
@@ -0,0 +1,7 @@
+import 'package:mobx/mobx.dart';
+
+part 'matricula_store.g.dart';
+
+class MatriculaStore = MatriculaStoreBase with _$MatriculaStore;
+
+abstract class MatriculaStoreBase with Store {}
